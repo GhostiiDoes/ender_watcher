ab6b24716845aca58cacfc5eba0e1d6cb4c80079
Ghostplays10 <ghostplays10@gmail.com>
2023-06-24 17:01:55 -0300

    Vanilla Version

---
+++ src/main/java/ghostiidoes/ender_watcher/EnderWatcherClient.java
@@ -0,0 +1,10 @@
+package ghostiidoes.ender_watcher;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnderWatcherClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
