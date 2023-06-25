859a96cc95a2f62b90a4cf9f9d3da5b9bf02e192
Ghostplays10 <ghostplays10@gmail.com>
2023-06-25 20:20:37 -0300

    Watcher now gets angered and teleports if it has spent too much time near the player (and the player looks at them).
    Still doesn't have any sound, no spawn egg, doesn't vanish when hit with water bottles and doesn't move blocks that are on the way.

--- src/main/java/ghostiidoes/ender_watcher/entity/custom/WatcherEntity.java
+++ src/main/java/ghostiidoes/ender_watcher/entity/custom/WatcherEntity.java
@@ -1,75 +1,424 @@
 package ghostiidoes.ender_watcher.entity.custom;
 
-import net.minecraft.entity.EntityType;
+import ghostiidoes.ender_watcher.EnderWatcherMod;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.*;
+import net.minecraft.entity.ai.TargetPredicate;
 import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.ai.pathing.PathNodeType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.mob.Angerable;
-import net.minecraft.entity.mob.HostileEntity;
-import net.minecraft.entity.passive.ChickenEntity;
-import net.minecraft.entity.passive.MerchantEntity;
-import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.mob.*;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.entity.projectile.thrown.PotionEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionUtil;
+import net.minecraft.potion.Potions;
+import net.minecraft.registry.tag.DamageTypeTags;
+import net.minecraft.registry.tag.FluidTags;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.TimeHelper;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.GeoAnimatable;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.*;
+import software.bernie.geckolib.core.animation.AnimationState;
 import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.function.Predicate;
 
 public class WatcherEntity extends HostileEntity implements Angerable, GeoEntity {
+
+    private static final UUID ATTACKING_SPEED_BOOST_ID = UUID.fromString("020E0DFB-87AE-4653-9556-831010E291A0");
+    private static final EntityAttributeModifier ATTACKING_SPEED_BOOST = new EntityAttributeModifier(ATTACKING_SPEED_BOOST_ID, "Attacking speed boost", (double)0.15f, EntityAttributeModifier.Operation.ADDITION);
+
+    private static final TrackedData<Optional<BlockState>> CARRIED_BLOCK = DataTracker.registerData(WatcherEntity.class, TrackedDataHandlerRegistry.OPTIONAL_BLOCK_STATE);
+    private static final TrackedData<Boolean> ANGRY = DataTracker.registerData(WatcherEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+    private static final TrackedData<Boolean> PROVOKED = DataTracker.registerData(WatcherEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+    private static final UniformIntProvider ANGER_TIME_RANGE = TimeHelper.betweenSeconds(60, 79);
+    private int lastAngrySoundAge = Integer.MIN_VALUE;
+    private int angerTime;
+    @Nullable
+    private UUID angryAt;
+
+    // Constructor + attributes
     public WatcherEntity(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
+        this.setStepHeight(1.0f);
+        this.setPathfindingPenalty(PathNodeType.WATER, -1.0f);
     }
 
     public static DefaultAttributeContainer.Builder setAttributes() {
-        return HostileEntity.createMobAttributes()
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, 60.0D)
-                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 7.0f)
-                .add(EntityAttributes.GENERIC_ATTACK_SPEED, 2.0f)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.35f);
+        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 80.0)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.3f)
+                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 7.0)
+                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 64.0);
     }
+    ////////////////////
+    // AI
 
     @Override
     protected void initGoals() {
-        this.goalSelector.add(1, new SwimGoal(this));
-        this.goalSelector.add(2, new MeleeAttackGoal(this, 1.2D, false));
-        this.goalSelector.add(3, new WanderAroundFarGoal(this, 0.75f, 1));
+        this.goalSelector.add(0, new SwimGoal(this));
+        this.goalSelector.add(1, new ChasePlayerGoal(this));
+        this.goalSelector.add(2, new MeleeAttackGoal(this, 1.0, false));
+        this.goalSelector.add(7, new WanderAroundFarGoal((PathAwareEntity)this, 1.0, 0.0f));
+        this.goalSelector.add(8, new LookAtEntityGoal(this, PlayerEntity.class, 8.0f));
+        this.goalSelector.add(8, new LookAroundGoal(this));
+        this.targetSelector.add(1, new TeleportTowardsPlayerGoal(this, this::shouldAngerAt));
+        this.targetSelector.add(2, new RevengeGoal(this, new Class[0]));
+        this.targetSelector.add(3, new ActiveTargetGoal<EndermiteEntity>((MobEntity)this, EndermiteEntity.class, true, false));
+        this.targetSelector.add(4, new UniversalAngerGoal<WatcherEntity>(this, false));
+    }
 
-        this.goalSelector.add(4, new LookAroundGoal(this));
+    @Override
+    public void setTarget(@Nullable LivingEntity target) {
+        super.setTarget(target);
+        EntityAttributeInstance entityAttributeInstance = this.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
+        int ageWhenTargetSet;
+        if (target == null) {
+            ageWhenTargetSet = 0;
+            this.dataTracker.set(ANGRY, false);
+            this.dataTracker.set(PROVOKED, false);
+            entityAttributeInstance.removeModifier(ATTACKING_SPEED_BOOST);
+        } else {
+            ageWhenTargetSet = this.age;
+            this.dataTracker.set(ANGRY, true);
+            if (!entityAttributeInstance.hasModifier(ATTACKING_SPEED_BOOST)) {
+                entityAttributeInstance.addTemporaryModifier(ATTACKING_SPEED_BOOST);
+            }
+        }
+    }
 
-        this.targetSelector.add(2, new ActiveTargetGoal<>(this, PlayerEntity.class, true));
+    @Override
+    public boolean hurtByWater() { return true; }
+
+    @Override
+    protected void initDataTracker() {
+        super.initDataTracker();
+        this.dataTracker.startTracking(CARRIED_BLOCK, Optional.empty());
+        this.dataTracker.startTracking(ANGRY, false);
+        this.dataTracker.startTracking(PROVOKED, false);
+    }
+
+    @Override
+    public boolean damage(DamageSource source, float amount) {
+        if (this.isInvulnerableTo(source)) {
+            return false;
+        }
+        boolean bl = source.getSource() instanceof PotionEntity;
+        if (source.isIn(DamageTypeTags.IS_PROJECTILE) || bl) {
+            boolean bl2 = bl && this.damageFromPotion(source, (PotionEntity)source.getSource(), amount);
+            for (int i = 0; i < 64; ++i) {
+                if (!this.teleportRandomly()) continue;
+                return true;
+            }
+            return bl2;
+        }
+        boolean bl2 = super.damage(source, amount);
+        if (!this.getWorld().isClient() && !(source.getAttacker() instanceof LivingEntity) && this.random.nextInt(10) != 0) {
+            this.teleportRandomly();
+        }
+        return bl2;
+    }
+
+    private boolean damageFromPotion(DamageSource source, PotionEntity potion, float amount) {
+        boolean bl;
+        ItemStack itemStack = potion.getStack();
+        Potion potion2 = PotionUtil.getPotion(itemStack);
+        List<StatusEffectInstance> list = PotionUtil.getPotionEffects(itemStack);
+        boolean bl2 = bl = potion2 == Potions.WATER && list.isEmpty();
+        if (bl) {
+            return super.damage(source, amount);
+        }
+        return false;
+    }
+
+    @Override
+    protected float getActiveEyeHeight(EntityPose pose, EntityDimensions dimensions) {
+        return 4.3f;
+    }
+
+    public void setProvoked() {
+        this.dataTracker.set(PROVOKED, true);
     }
 
     @Override
     public int getAngerTime() {
-        return 0;
+        return this.angerTime;
     }
 
     @Override
     public void setAngerTime(int angerTime) {
+        this.angerTime = angerTime;
+    }
 
+    public boolean isProvoked() {
+        return this.dataTracker.get(PROVOKED);
+    }
+    public boolean isAngry() {
+        return this.dataTracker.get(ANGRY);
     }
 
     @Nullable
     @Override
     public UUID getAngryAt() {
-        return null;
+        return this.angryAt;
+    }
+
+    @Override
+    public void onTrackedDataSet(TrackedData<?> data) {
+        if (ANGRY.equals(data) && this.isProvoked() && this.getWorld().isClient) {
+            this.playAngrySound();
+        }
+        super.onTrackedDataSet(data);
+    }
+
+    public void playAngrySound() {
+        if (this.age >= this.lastAngrySoundAge + 400) {
+            this.lastAngrySoundAge = this.age;
+            if (!this.isSilent()) {
+                this.getWorld().playSound(this.getX(), this.getEyeY(), this.getZ(), SoundEvents.ENTITY_ENDERMAN_STARE, this.getSoundCategory(), 2.5f, 1.0f, false);
+            }
+        }
+    }
+
+    protected boolean teleportRandomly() {
+        if (this.getWorld().isClient() || !this.isAlive()) {
+            return false;
+        }
+        double d = this.getX() + (this.random.nextDouble() - 0.5) * 64.0;
+        double e = this.getY() + (double)(this.random.nextInt(64) - 32);
+        double f = this.getZ() + (this.random.nextDouble() - 0.5) * 64.0;
+        return this.teleportTo(d, e, f);
+    }
+
+    boolean teleportTo(Entity entity) {
+        Vec3d vec3d = new Vec3d(this.getX() - entity.getX(), this.getBodyY(0.5) - entity.getEyeY(), this.getZ() - entity.getZ());
+        vec3d = vec3d.normalize();
+        double d = 16.0;
+        double e = this.getX() + (this.random.nextDouble() - 0.5) * 8.0 - vec3d.x * 16.0;
+        double f = this.getY() + (double)(this.random.nextInt(16) - 8) - vec3d.y * 16.0;
+        double g = this.getZ() + (this.random.nextDouble() - 0.5) * 8.0 - vec3d.z * 16.0;
+        return this.teleportTo(e, f, g);
+    }
+
+    private boolean teleportTo(double x, double y, double z) {
+        BlockPos.Mutable mutable = new BlockPos.Mutable(x, y, z);
+        while (mutable.getY() > this.getWorld().getBottomY() && !this.getWorld().getBlockState(mutable).blocksMovement()) {
+            mutable.move(Direction.DOWN);
+        }
+        BlockState blockState = this.getWorld().getBlockState(mutable);
+        boolean bl = blockState.blocksMovement();
+        boolean bl2 = blockState.getFluidState().isIn(FluidTags.WATER);
+        if (!bl || bl2) {
+            return false;
+        }
+        Vec3d vec3d = this.getPos();
+        boolean bl3 = this.teleport(x, y, z, true);
+        if (bl3) {
+            this.getWorld().emitGameEvent(GameEvent.TELEPORT, vec3d, GameEvent.Emitter.of(this));
+            if (!this.isSilent()) {
+                this.getWorld().playSound(null, this.prevX, this.prevY, this.prevZ, SoundEvents.ENTITY_ENDERMAN_TELEPORT, this.getSoundCategory(), 1.0f, 1.0f);
+                this.playSound(SoundEvents.ENTITY_ENDERMAN_TELEPORT, 1.0f, 1.0f);
+            }
+        }
+        return bl3;
     }
 
     @Override
     public void setAngryAt(@Nullable UUID angryAt) {
-
+        this.angryAt = angryAt;
     }
 
     @Override
-    public void chooseRandomAngerTime() {
+    public void chooseRandomAngerTime()  {
+        this.setAngerTime(ANGER_TIME_RANGE.get(this.random));
+    }
 
+    boolean isPlayerSeeing(PlayerEntity player) {
+        ItemStack itemStack = player.getInventory().armor.get(3);
+        if (itemStack.isOf(Blocks.CARVED_PUMPKIN.asItem())) {
+            return false;
+        }
+        else if (player.canSee(this)) {
+            Vec3d playerRotation = player.getRotationVec(1.0f).normalize();
+            Vec3d watcherPosition = new Vec3d(this.getX() - player.getX(), this.getEyeY() - player.getEyeY() - 2.0f, this.getZ() - player.getZ());
+            double distance = watcherPosition.length();
+            double e = playerRotation.dotProduct(watcherPosition = watcherPosition.normalize());
+            if (e < 1.0 - 0.6 / distance) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+
+    boolean isPlayerStaring(PlayerEntity player) {
+        ItemStack itemStack = player.getInventory().armor.get(3);
+        if (itemStack.isOf(Blocks.CARVED_PUMPKIN.asItem())) {
+            return false;
+        }
+        Vec3d vec3d = player.getRotationVec(1.0f).normalize();
+        Vec3d vec3d2 = new Vec3d(this.getX() - player.getX(), this.getEyeY() - player.getEyeY(), this.getZ() - player.getZ());
+        double d = vec3d2.length();
+        double e = vec3d.dotProduct(vec3d2 = vec3d2.normalize());
+        if (e > 1.0 - 0.025 / d) {
+            return player.canSee(this);
+        }
+        return false;
+    }
+
+    ///////////////
+    // CUSTOM GOALS
+
+    static class ChasePlayerGoal extends Goal {
+        private final WatcherEntity ender_watcher;
+        @Nullable
+        private LivingEntity target;
+
+        public ChasePlayerGoal(WatcherEntity ender_watcher) {
+            this.ender_watcher = ender_watcher;
+            this.setControls(EnumSet.of(Goal.Control.JUMP, Goal.Control.MOVE));
+        }
+
+        @Override
+        public boolean canStart() {
+            this.target = this.ender_watcher.getTarget();
+            if (!(this.target instanceof PlayerEntity)) {
+                return false;
+            }
+            double d = this.target.squaredDistanceTo(this.ender_watcher);
+            if (d > 256.0) {
+                return false;
+            }
+            return this.ender_watcher.isPlayerStaring((PlayerEntity)this.target);
+        }
+
+        @Override
+        public void start() {
+            this.ender_watcher.getNavigation().stop();
+        }
+
+        @Override
+        public void tick() {
+            this.ender_watcher.getLookControl().lookAt(this.target.getX(), this.target.getEyeY(), this.target.getZ());
+        }
+    }
+
+    static class TeleportTowardsPlayerGoal extends ActiveTargetGoal<PlayerEntity> {
+        private final WatcherEntity ender_watcher;
+        @Nullable
+        private PlayerEntity targetPlayer;
+        private int lookAtPlayerWarmup;
+        private int ticksSinceUnseenTeleport;
+        private int proximityTime;
+        private final TargetPredicate staringPlayerPredicate;
+        private final TargetPredicate validTargetPredicate = TargetPredicate.createAttackable().ignoreVisibility();
+        private final Predicate<LivingEntity> angerPredicate;
+
+        public TeleportTowardsPlayerGoal(WatcherEntity ender_watcher, @Nullable Predicate<LivingEntity> targetPredicate) {
+            super(ender_watcher, PlayerEntity.class, 10, false, false, targetPredicate);
+            this.ender_watcher = ender_watcher;
+            this.angerPredicate = playerEntity -> (ender_watcher.isPlayerStaring((PlayerEntity) playerEntity) || ender_watcher.shouldAngerAt((LivingEntity) playerEntity)) && !ender_watcher.hasPassengerDeep((Entity) playerEntity);
+            this.staringPlayerPredicate = TargetPredicate.createAttackable().setBaseMaxDistance(this.getFollowRange()).setPredicate(this.angerPredicate);
+        }
+
+        @Override
+        public boolean canStart() {
+            this.targetPlayer = this.ender_watcher.getWorld().getClosestPlayer(this.staringPlayerPredicate, this.ender_watcher);
+            return this.targetPlayer != null;
+        }
+
+        @Override
+        public void start() {
+            this.lookAtPlayerWarmup = this.getTickCount(5);
+            this.ticksSinceUnseenTeleport = 0;
+            this.ender_watcher.setProvoked();
+        }
+
+        @Override
+        public void stop() {
+            this.targetPlayer = null;
+            super.stop();
+        }
+
+        @Override
+        public boolean shouldContinue() {
+            if (this.targetPlayer != null) {
+                if (!this.angerPredicate.test(this.targetPlayer)) {
+                    return false;
+                }
+                this.ender_watcher.lookAtEntity(this.targetPlayer, 10.0f, 10.0f);
+                return true;
+            }
+            if (this.targetEntity != null) {
+                if (this.ender_watcher.hasPassengerDeep(this.targetEntity)) {
+                    return false;
+                }
+                if (this.validTargetPredicate.test(this.ender_watcher, this.targetEntity)) {
+                    return true;
+                }
+            }
+            return super.shouldContinue();
+        }
+
+        @Override
+        public void tick() {
+            if (this.ender_watcher.getTarget() == null) {
+                super.setTargetEntity(null);
+            }
+            if (this.targetPlayer != null) {
+                if (--this.lookAtPlayerWarmup <= 0) {
+                    this.targetEntity = this.targetPlayer;
+                    this.targetPlayer = null;
+                    super.start();
+                }
+            } else {
+                if (this.targetEntity != null && !this.ender_watcher.hasVehicle()) {
+                    if (this.targetEntity.distanceTo(this.ender_watcher) < 10) {
+                        if (this.proximityTime < 20) {
+                            this.proximityTime++;
+                        }
+                        if (this.proximityTime >= 20 && ender_watcher.isPlayerSeeing((PlayerEntity)this.targetEntity)) {
+                            if (this.ender_watcher.teleportRandomly()) {
+                                this.proximityTime = 0;
+                            }
+                        }
+                        EnderWatcherMod.LOGGER.info("Seen time = " + this.proximityTime);
+                        this.ticksSinceUnseenTeleport = 0;
+                    } else if (this.targetEntity.squaredDistanceTo(this.ender_watcher) > 256.0 && this.ticksSinceUnseenTeleport++ >= this.getTickCount(30) && this.ender_watcher.teleportTo(this.targetEntity)) {
+                        this.ticksSinceUnseenTeleport = 0;
+                    }
+                }
+                super.tick();
+            }
+        }
     }
 
     ///////////////
